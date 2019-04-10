dfad9f9a278c18db8590805de661b579616d4bb8
Fabio Albertin <albertin@gmail.com>
2019-04-10 21:42:24 +0100

    Starting Redux integration, updated README

---
+++ src/store/configureStore.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import thunk from 'redux-thunk'
+import createHistory from 'history/createBrowserHistory';
+
