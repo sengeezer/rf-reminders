8b1c6d5ca48ed00727afa36d1fc57009cd845e74
Fabio Albertin <albertin@gmail.com>
2019-04-11 18:30:49 +0100

    Finished about and routing

---
+++ src/components/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h2>About Us</h2>
+
+    <p>Hello Medium!</p>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import store, { history } from './store/configureStore';
+
 import App from './components/app';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div><App /></div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// If you want your app to work offline and load faster,
+// you can change unregister() to register()
 serviceWorker.unregister();
