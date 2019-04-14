a341641decd0a071c559ee3382e5e20cd8780a10
Fabio Albertin <albertin@gmail.com>
2019-04-14 02:33:47 +0100

    Added react-calender

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
 
-export default () => (
-  <div>
-    <h2>About Us</h2>
+export default class About extends Component {
+  state = {
+    value: new Date(),
+  }
+  onChange(value) {
+    this.setState({ value });
+  }
+  render() {
+    const { value } = this.state;
 
-    <p>Hello Medium!</p>
-  </div>
-);
+    return (
+      <div>
+        <h2>About Us</h2>
+
+        <Calendar
+            onChange={this.onChange}
+            showWeekNumbers
+            value={value}
+        />
+      </div>
+    );
+  }
+}
