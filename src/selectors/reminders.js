ab19bb7cb9f8d9fc71c95ed776f3c9dc5f640754
Fabio Albertin <albertin@gmail.com>
2019-04-21 01:31:45 +0100

    Added filters

---
+++ src/selectors/reminders.js
@@ -0,0 +1,21 @@
+import moment from 'moment';
+
+const getMatchedReminders = (reminders, { text, sortBy, date }) => {
+  return reminders.filter((reminder) => {
+    const reminderDate = moment(reminder.date);
+    const compareDate = moment(date);
+
+    const dateMatch = compareDate ? compareDate.isSame(reminderDate, 'day') : true;
+    const textMatch = typeof text !== 'string' || reminder.text.toLowerCase().includes(text.toLowerCase());
+
+    return dateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    }
+
+    return undefined;
+  });
+}
+
+export default getMatchedReminders;
